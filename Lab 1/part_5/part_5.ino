c63e210be282dc1cc6c56f1bf000a5beac8a6b5a
S. Nordin Abouzahra <abouzahra.9@wright.edu>
2018-09-09 01:48:44 -0400

    Lab 1, part 5: Completed!
    Lab 1, part 5 is fully completed per specification.
    There are various TODOs in the code for possible improvements.

--- Lab 1/part_5/part_5.ino
+++ Lab 1/part_5/part_5.ino
@@ -1,94 +1,200 @@
-// https://playground.arduino.cc/code/keypad
+/**
+ * Lab 1, Part 5 (30%)
+ * Combine the previous two projects. The keypad is used to enter a new song
+ * segment which replaces a previous song segment. You need to support the input
+ * of tones at least from C to high C. The push-button still controls whether to
+ * play the song. Demonstrate the ability to repetitively enter and play new
+ * song
+ * segments√√.
+ *
+ * Conditions:
+ * Hold the button for at least 1.75 second or change the note duration and
+ * delay.
+ * Toggled the song to stop playing and press the pound symbol on the keypad to
+ * program in a replacement composition.
+ *
+ * Remember: Show your lab instructor!
+ */
 
+/* Macros to make easy changes. */
+#define BTN_PIN 12
+#define LED_PIN LED_BUILTIN
+
+/* Library taken from: <https://playground.arduino.cc/code/keypad> */
 #include <Keypad.h>
-
 const byte ROWS = 4;
 const byte COLS = 3;
 char keys[ROWS][COLS] = {
-  {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'}
-};
+    {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'}};
 byte rowPins[ROWS] = {5, 6, 7, 8};
 byte colPins[COLS] = {2, 3, 4};
-
 Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-int note;
-double pitch;
-unsigned long loop_count;
-unsigned long start_time;
-byte speaker = 11;
-int note_vals[] = {0, 2, 4, 5, 7, 9, 11, 12};
-int octive = 1;
-const int touch_sensor_pin = -1;
-const int button_pin = 12;
-int touch_sensor_pressed = 0;
-int lasttouch_sensor_state = 0;
-int touch_sensor_state = 0;
-int touch_sensor_released = 1;
-unsigned long last_debounce_time = 0;
-unsigned long debounce_delay = 10;
 
 /* btn { pressed, state, prev_state, pin } */
 struct btn_s {
-  int       pressed;
-  int       state;
-  int       prev_state;
+  int pressed;
+  int state;
+  int prev_state;
   const int pin;
-} btn_def = {1, 1, 1, 12};
+} btn_def = {1, 1, 1, BTN_PIN};
 typedef struct btn_s btn;
 
+/* led { state, pin } */
+struct led_s {
+  int state;
+  const int pin;
+} led_def = {0, LED_PIN};
+typedef struct led_s led;
+
+/* debounce { delay, last_time } */
+struct debounce_s {
+  const unsigned delay;
+  unsigned long last_time; /* old millis() */
+} dbn_def = {10, 0};
+typedef debounce_s dbn;
+
+/* sound { output, playing, position }*/
+struct sound_s {
+  const byte output;
+  bool playing;
+  uint8_t position;
+} snd_def = {11, false, 0};
+typedef sound_s snd;
+
+/* music { composition, frequency, duration, octave, position } */
+struct music_s {
+  char composition[8];
+  double frequency[8];
+  uint8_t duration[8];
+  uint8_t octave;
+  uint8_t position;
+} msc_def = {{'h', 'd', 'd', 'f', 'a', 'f', 'g', 'h'},
+             {},
+             {16, 4, 8, 16, 4, 8, 16, 16},
+             1,
+             0};
+typedef music_s msc;
+
+/* Initalize structs with default values. */
+btn *Toggle   = &btn_def;
+led *LED      = &led_def;
+dbn *Debounce = &dbn_def;
+snd *Sound    = &snd_def;
+msc *Music    = &msc_def;
+
+uint8_t get_pitch(char note) {
+  static const uint8_t lookup[8] =
+  { 0x9 /* a */
+  , 0xB /* b */
+  , 0x0 /* c */
+  , 0x2 /* d */
+  , 0x4 /* e */
+  , 0x5 /* f */
+  , 0x7 /* g */
+  , 0xC /* h (High C) */
+  };      /* Numerical pitch mapped to note. */
+  if (note > '8')
+    return lookup[note - 'a'];
+  return lookup[note - '1'];
+}
+
+/**
+ * update_music(char*) - Given a char ptr, generate associated frequencies.
+ * TODO:
+ * We might want to also generate the higher octave frequencies by default.
+ * This would allow us to not have more abstraction logic for dealing with
+ * the touch sensor.
+ */
+void update_music(char *composition) {
+  for (int i = 0; i < 8; i++) {
+    uint8_t pitch = /* octave * */get_pitch(composition[i]);
+    Music->frequency[i] = 261.2 * pow(2, pitch / 12.);
+  }
+  Sound->position = 0; /* Sound device should start anew. */
+  Music->position = 0; /* Ya, know I am not sure... */
+}
+
+void play_music() {
+  uint8_t length = Music->duration[Sound->position];
+  tone(Sound->output, Music->frequency[Sound->position++], 70 * length);
+  delay(10 * length);
+  noTone(Sound->output);
+  if (Sound->position > 7)
+    Sound->position = 0;
+}
+
+void print_music_composition() {
+  for (int i = 0; i < 8; i++) {
+    Serial.print(Music->composition[i]);
+  }
+  Serial.println();
+}
 
 void setup() {
+  pinMode(Toggle->pin, INPUT_PULLUP);
+  pinMode(LED->pin, OUTPUT);
+  digitalWrite(LED->pin,
+               LED->state); /* Our inital default for the LED is off, i.e. 0*/
+  update_music(Music->composition); /* Generates default frequencies. */
   Serial.begin(9600);
-  loop_count = 0;
-  start_time = millis();
-  pinMode(touch_sensor_pin, INPUT_PULLUP);
 }
+
 void loop() {
-  loop_count++;
+  Toggle->pressed = digitalRead(Toggle->pin);
 
-  touch_sensor_state = digitalRead(touch_sensor_pin);
+  if ((Toggle->prev_state) != (Toggle->pressed)) {
+    /* If the last button state does not match the current reading, update
+     * last debounce time. */
+    Debounce->last_time = millis();
+  }
 
-  touch_sensor();
-  if (kpd.getKeys()) {
-    for (int i = 0; i < LIST_MAX; i++) {
-      if (kpd.key[i].stateChanged) {
-        Serial.println("Key pressed.");
-        if (kpd.key[i].kchar >= '1' && kpd.key[i].kchar <= '8') {
-          note = kpd.key[i].kchar - '1';
-          pitch = octive * 261.2 * pow(2.0, (note_vals[note] / 12.0));
-          switch (kpd.key[i].kstate) {
-            case PRESSED:
-              tone(speaker, pitch);
-              break;
-            case HOLD:
-              break;
-            case RELEASED:
-              noTone(speaker);
-              break;
-            case IDLE:
-              break;
-            default:
-              break;
-          }
-        }
+  /* currentUptime - oldUptime > delay */
+  if ((millis() - (Debounce->last_time)) > (Debounce->delay)) {
+    /* The button is still changed after delay. */
+    if (Toggle->state != Toggle->pressed) {
+      Toggle->state = Toggle->pressed;
+      if (Toggle->state) {
+        LED->state = !LED->state;
+        Sound->playing = !Sound->playing;
       }
     }
   }
-}
 
-void button() {
+  digitalWrite(LED->pin, LED->state);
 
-}
+  /**
+   * The following code physically means, if the button toggles the sound off.
+   * If the user then presses the pound key on the keypad, then read in 8 keys.
+   * waitForKey() is a blocking call.
+   *
+   * TODO: Use a for-loop.
+   */
+  if (!Sound->playing) {
+    noTone(Sound->output); /* TODO: does this improve the responsiveness? */
+    char key = kpd.getKey();
+    if (key == '#') {
+      Music->composition[0] = kpd.waitForKey();
+      Music->composition[1] = kpd.waitForKey();
+      Music->composition[2] = kpd.waitForKey();
+      Music->composition[3] = kpd.waitForKey();
+      Music->composition[4] = kpd.waitForKey();
+      Music->composition[5] = kpd.waitForKey();
+      Music->composition[6] = kpd.waitForKey();
+      Music->composition[7] = kpd.waitForKey();
 
-void touch_sensor() {
-  int reading = digitalRead(touch_sensor_pin);
-  while (reading == LOW) {
-    delay(10);
-    reading = digitalRead(touch_sensor_pin);
-    if (reading != LOW) {
-      Serial.println("Octave change.");
-      octive = (octive == 1) ? 2 : 1;
+      update_music(Music->composition);
+      Serial.println("Music composition updated to: ");
+      print_music_composition();
     }
   }
-}
 
+  /**
+   * TODO: We might want to look into adding the octave support here.
+   * if (Sound->player && digitalRead(touch_sensor) == HIGH) ...
+   */
+  if (Sound->playing) {
+    play_music();
+  }
+
+  Toggle->prev_state = Toggle->pressed;
+}
