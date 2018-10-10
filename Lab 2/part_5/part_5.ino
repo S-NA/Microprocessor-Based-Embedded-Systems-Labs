ac66bf6c1a3a8b8df29f26fb8821204a2760c895
S. Nordin Abouzahra <abouzahra.9@wright.edu>
2018-10-10 16:25:15 -0400

    Lab 2, part 5: Add prelab draft.

--- Lab 2/part_5/part_5.ino
+++ Lab 2/part_5/part_5.ino
@@ -9,7 +9,7 @@ struct signal_generator_s {
   unsigned long pwm_low;
   unsigned long period;
   float frequency;
-  unsigned long duty_cycle;
+  unsigned long duty_cycle; /* likely uneeded */
   const byte pin;
 } signal_generator_def = { 0, 0, 0, 0, 0, 8 };
 typedef struct signal_generator_s signal_generator;
@@ -20,8 +20,9 @@ struct led_s {
   unsigned long period;
   float frequency;
   unsigned long duty_cycle;
+  unsigned long currentTime;
   const byte pin;
-} led_def = { 0, 0, 0, 0, 0, 8 };
+} led_def = { 0, 0, 0, 0, 0, 0, 8 };
 typedef struct led_s led;
 
 force_resistor *ForceResistor = &force_resistor_def;
@@ -32,21 +33,26 @@ void setup() {
   Serial.begin(9600);
   pinMode(ForceResistor->pin, INPUT);
   pinMode(SignalGenerator->pin, INPUT);
-  TCCR1A = _BV(COM1B1) | _BV(WGM11) | _BV(WGM11); /* mode */
-  TCCR1B = _BV(CS11)   | _BV(WGM13) | _BV(WGM12); /* mode */
-
-  ICR1 = 0x07D0; /* 2000 */
-  OCR1B = 0x02FE; /* 766 */
+  pinMode(LED->pin, OUTPUT);
+  LED->currentTime = millis(); /* zero... */
 }
 
 void loop() {
+  /* Force resistor value increases with force to it, used for analogWrite() */
   ForceResistor->value = map(analogRead(ForceResistor->pin), 0, 1023, 0, 255);
-  /* Signal Generator */
+  /* Signal generator, we will likely sample more to get a more accurate freq before.  */
   SignalGenerator->pwm_high = pulseIn(SignalGenerator->pin, HIGH);
   SignalGenerator->pwm_low = pulseIn(SignalGenerator->pin, LOW);
   SignalGenerator->period = (SignalGenerator->pwm_low + SignalGenerator->pwm_high);
   SignalGenerator->frequency = (1 / (SignalGenerator->period / 1000000.0));
+  /* Duty cycle uneeded, keep in case we deal with this using registers instead of analogWrite() */
   SignalGenerator->duty_cycle = (float(SignalGenerator->pwm_high) / float(SignalGenerator->period)) * 100.0;
+
+  /* if (currentTime + (500 / (SignalGenerator->frequency / 1000) > millis() ?... 
+   *  analogWrite(SignalGenerator->pin, ForceResistor->value);
+   *  on ? off : logic
+   *  currentTime = millis()
+   */
 
   Serial.println(ForceResistor->value);
   Serial.println(SignalGenerator->frequency);
